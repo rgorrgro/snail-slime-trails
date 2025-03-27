3c071245dba901f296c585ded51c4ab20ba7aa5e
Robert Gorr-Grohmann <106992082+rgorrgro@users.noreply.github.com>
2025-03-27 16:30:44 +0100

    Update 250318 genHelp.js

--- genHelp.js
+++ genHelp.js
@@ -9,6 +9,7 @@ class Help {
     this.btst = false;
     this.btstAut = false;
     this.btstConf = false;
+    this.btstRuntime = false;
 //   color
   this.colorArray = [
   ["red",[255,0,0,255]],
@@ -25,7 +26,7 @@ class Help {
   ["black",[0,0,0,255]]
 ];
 //   format
-this.displayformatArray = [
+  this.displayformatArray = [
   ["A4-Portrait",15,22],
   ["A4-Landscape",22,15],
   ["Widescreen-P",12,19],
@@ -33,11 +34,16 @@ this.displayformatArray = [
   ["Smartphone-P",9,16],
   ["Smartphone-L",16,9]
 ];
+// Snailtypes
+  this.snailtypeArray = [
+    ["All",0],
+    ["One",1]
+  ];  
   }
 //
-//  Check Configuration
+//  Check and set configuration
 //
-  chkConfFormat() {
+  chkDisplayformatAndGeometry() {
     let h0, h1, h2;
     // Canvas width and height
     let w0 = int(innerWidth-4);    
@@ -45,50 +51,95 @@ this.displayformatArray = [
     let nr = 0;
     for(let i=0;i<this.displayformatArray.length;i++) {
       if (this.displayformatArray[i][0] 
-          ==displayformat[0]) {
+          ==displayformat.val[0]) {
         nr=i;
       }
     }
-    let x = w0 / this.displayformatArray[nr][1];
-    if (x*this.displayformatArray[nr][2]>h0) {
-      x = h0 / this.displayformatArray[nr][2];
+    let x = w0 / displayformat.val[1];
+    if (x*displayformat.val[2]>h0) {
+      x = h0 / displayformat.val[2];
       canvaswidth = int(
-        x*this.displayformatArray[nr][1]);
+        x*displayformat.val[1]);
       canvasheight = h0;
     } else {
       canvaswidth = w0;
       canvasheight = int(
-        x*this.displayformatArray[nr][2]);
+        x*displayformat.val[2]);
     }
+    hlp.tstConf("displayformat|X|Y|W|H",
+                displayformat.val[0],
+                displayformat.val[1],
+                displayformat.val[2],
+                canvaswidth,
+                canvasheight);
+    this.chkRectcntx(true);
   }
-  XchkConfRectangle() {
-    let h0, h1, h2;
-    // Count rectangles in X and Y and the size
-    rectsize = int(canvaswidth/cntrectx.val-1);
-    cntrecty = int(canvasheight/rectsize);
-    this.tstConf("Count X|Y|Size",
-                 cntrectx.val,cntrecty,rectsize);
-    // Compute cntralization vector
-    h0 = (canvaswidth-(cntrectx*rectsize))/2;
-    h1 = (canvasheight-(cntrecty*rectsize))/2;
-    addv = createVector(h0,h1);
+  chkRectcntx(setval_) {
+    if (setval_) {
+      rectcntx = new SliderValue("rectcntx",6,2,
+                    int(canvaswidth/rectsizemin-1));
+    }
+    rectsize = int(canvaswidth/rectcntx.val-1);
+    rectcnty = int(canvasheight/rectsize);
+    hlp.tstConf("rectcntx|rectcnty|rectsize",
+                rectcntx.val,rectcnty,rectsize);
+    this.chkLinesize(true);
   }
-  chkConfColor() {
-    let h0, h1, h2;
-    // Colors
-    for (let i=0;i<this.colorArray.length;i++) {
-      if (this.colorArray[i][0]==bgcolor[0]) {
-        let j = (i+1==this.colorArray.length?0:i+1);
-        linecolor = this.colorArray[j].slice(0);
+  chkLinesize(setval_) {
+    if (setval_) {
+      linesize = new SliderValue("linesize",
+                  int(rectsize/6),1,int(rectsize/4));
+    }
+    hlp.tstConf("linesize",linesize.val);
+    this.chkCentralvector();
+  }
+  chkCentralvector() {
+    centralvector = createVector(
+      (canvaswidth-(rectcntx.val*rectsize))/2,
+      (canvasheight-(rectcnty*rectsize))/2);
+    hlp.tstConf("centralvector X|Y",
+                centralvector.x,centralvector.y);
+  }
+  chkSnailparams() {
+    snailtype = 
+      new RadioValue("snailtype",0,
+                     this.snailtypeArray);
+    snailmaxcnt = 
+      new SliderValue("snailmaxcnt",10,1,30);
+    snailminlen = 
+      new SliderValue("snailminlen",2,1,5);
+    hlp.tstConf("snailmaxcnt|snailminlen",
+               snailmaxcnt.val,snailminlen.val);    
+  }
+  chkColors(setval_) {
+    let rand = 0;
+    if (setval_) {
+      rand = int(random(this.colorArray.length));
+      bgcolor = 
+        new RadioValue("bgcolor",rand,this.colorArray);
+    } else {
+      for (let i=0;i<this.colorArray.length;i++) {
+        if (bgcolor[0]==this.colorArray[i]) {
+          rand = i;
+        }
       }
     }
+    rand = (rand+1==this.colorArray.length?0:rand+1);
+    linecolor = this.colorArray[rand].slice(0);
+    hlp.tstConf("linecolor",linecolor[0]);
+    hlp.tstConf("bgcolor",bgcolor.val[0]);
+  }
+  chkFrames() {
+    frames   = new SliderValue("frames",24,1,24);
+    frameRate(frames.val);
+    hlp.tstConf("frames",frames.val);
   }
 //
 // Colors
 //
   colNextLinecolor() {
     let i = int(random(this.colorArray.length));
-    if (this.colorArray[i][0]==bgcolor[0]) {
+    if (this.colorArray[i][0]==bgcolor.val[0]) {
       i = (i+1==this.colorArray.length?0:i+1);
     }
     linecolor = this.colorArray[i];
@@ -129,6 +180,17 @@ this.displayformatArray = [
   }
   tstConfOn() {this.btstConf = true;}
   tstConfOff() {this.btstConf = false;}
+  tstRuntime() {
+    if (this.btstRuntime) {
+      let s = "Runtime "+arguments[0]+": ";
+      for (let i=1;i<arguments.length;i++) {
+        s += arguments[i]+"|";
+      }
+      print (s);
+    }
+  }
+  tstRuntimeOn() {this.btstRuntime = true;}
+  tstRuntimeOff() {this.btstRuntime = false;}
 //
 //  Error handling
 //
@@ -148,9 +210,9 @@ this.displayformatArray = [
   }
 }
 //
-// Min-Max-Values
+// Slider-Values
 //
-class MinMaxValue {
+class SliderValue {
   constructor(name_,default_,min_,max_) {
     this.nam = name_;
     this.val = int(default_);
@@ -162,5 +224,18 @@ class MinMaxValue {
         ?this.min
         :(value_>this.max?this.max:int(value_)));
   }
-  tst(){return(this.nam+"="+this.val);}
+//  tst(){return(this.nam+"="+this.val);}
+}
+//
+// Radio-Values
+//
+class RadioValue {
+  constructor(name_,default_,array_) {
+    this.nam = name_;
+    this.arr = array_.slice(0);
+    this.val = this.arr[int(default_)].slice(0);
+  }
+  set(value_){
+    this.val = value_;
+  }
 }
